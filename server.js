1aab83f31356e89846a7ea265a4c340ce09ddeb0
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-01-25 15:59:38 -0800

    initial file structure

---
+++ server.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const express = require('express');
+require('dotenv').config();
+const superagent = require('superagent');
+const PORT = process.env.PORT || 3111;
+
+const app = express();
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.static('/public'));
+app.set('view engine', 'ejs');
+
+app.get('/', (request, response) => {
+  response.send('Made it home');
+});
+
+
+app.listen(PORT, () => console.log(`up on PORT ${PORT}`));
