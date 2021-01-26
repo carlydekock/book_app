216b05e27b383e43e1e78ce518fb5715cffd8297
Glenn <Elliron.Clark@gmail.com>
2021-01-25 17:38:25 -0800

    finished task 2

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3111;
 
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
@@ -15,8 +15,25 @@ app.get('/', (request, response) => {
   response.send('Made it home');
 });
 
-app.get('/hello', (request,response) => {
+app.get('/hello', (request, response) => {
   response.render('pages/index.ejs');
 });
+
+app.get('/searches/new', (request, response) => {
+  response.render('pages/searches/new.ejs');
+});
+
+app.post('/searches', makeBookSearch);
+
+function makeBookSearch(request, response) {
+  const title = request.body.title;
+  const url = `https://www.googleapis.com/books/v1/volumes?q=+intitle:${title}`;
+  superagent.get(url).then(stuff => {
+    console.log(stuff.body.items);
+    // const title = stuff.body.items.map(item => item.volumeInfo.title);
+    // response.render('pages/searches/new.ejs');
+    response.send('a string');
+  });
+}
 
 app.listen(PORT, () => console.log(`up on PORT ${PORT}`));
