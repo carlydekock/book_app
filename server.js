543b902b1c07e7d4f9aa8151a0fd67d775c450de
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-01-25 16:21:12 -0800

    updated styles files for proof of life test

--- server.js
+++ server.js
@@ -8,12 +8,15 @@ const PORT = process.env.PORT || 3111;
 const app = express();
 
 app.use(express.urlencoded({extended: true}));
-app.use(express.static('/public'));
+app.use(express.static('./public'));
 app.set('view engine', 'ejs');
 
 app.get('/', (request, response) => {
   response.send('Made it home');
 });
 
+app.get('/hello', (request,response) => {
+  response.render('pages/index.ejs');
+});
 
 app.listen(PORT, () => console.log(`up on PORT ${PORT}`));
